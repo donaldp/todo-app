03d5469d166f5285f22d38a6a8450ed81d470ec8
donald_p <donaldpakkies@gmail.com>
2019-05-23 00:32:26 +0200

    feat: update the ui

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,50 @@
 
 @section('content')
 <div class="container">
+  <div class="row justify-content-center">
+    <form class='auth-form' method="POST" action="{{ route('register') }}">
+      @csrf
+
+      <div class="form-group">
+        <label for="name">{{ __('Name') }}</label>
+
+          <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+          @error('name')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+          @enderror
+      </div>
+
+      <div class="form-group">
+        <label for="email">{{ __('E-Mail Address') }}</label>
+
+          <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+          @error('email')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+          @enderror
+      </div>
+
+      <div class="form-group">
+        <label for="password">{{ __('Password') }}</label>
+
+          <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password"s>
+          @error('password')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+          @enderror
+      </div>
+
+      <button type="submit" class="btn btn-primary">{{ __('Register') }}</button>
+    </form>
+  </div>
+</div>
+
+
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +117,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
