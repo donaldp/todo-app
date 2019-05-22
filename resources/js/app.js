b50842011893ef5754457c4e504edcb1bf27fff3
donald_p <donaldpakkies@gmail.com>
2019-05-23 00:33:02 +0200

    feat: register all components and plugins used within the application

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,12 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import vueKanban from 'vue-kanban';
+import Notifications from 'vue-notification'
+
+Vue.use(vueKanban);
+Vue.use(Notifications)
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -20,6 +26,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('tasks-component', require('./components/tasks/all.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +35,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+  el: '#app',
 });
